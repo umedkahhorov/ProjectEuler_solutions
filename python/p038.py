27462a20941860350a611aacd5a216b4ac6deae3
umed Kakhkhorov <ukahhoroff@gmail.com>
2025-02-22 19:49:27 +0100

    038

--- python/p038.py
+++ python/p038.py
@@ -49,6 +49,18 @@ for i in range(2,10):
 
 print(best)
 
+from itertools import count, imap 
+def ispandigital( seed):
+   digits = str( seed)
+   for i in count(2):
+      ndig = len( digits)
+      if ndig < 9: digits += str( seed * i)
+      elif ndig == 9 and set( digits) == set( '123456789'):
+         return int( digits)
+      else: return False
+
+    print (max( imap( ispandigital, xrange( 1, 10000))))
+
 
 if __name__ == "__main__":
     t1 = time.time()
