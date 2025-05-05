474ed1f5b86005d01d55ee321b4a73278eccc02a
umed Kakhkhorov <ukahhoroff@gmail.com>
2025-05-05 19:38:15 +0200

    need to fix 47

--- python/p003.py
+++ python/p003.py
@@ -15,14 +15,16 @@ def prime_factorization_by_division():
     prime_factors.append(c)
     while(n > 1):
         if(n % c == 0):
-            print(c, end=" ")
+            #print(c, end=" ")
             n = n / c
             prime_factors.append(c) # only keep prime factors
         else:
             c = c + 1
             #prime_factors.append(c) # all factors
-    maxF = max(np.array(prime_factors))
-    return maxF
+    #maxF = max(np.array(prime_factors))
+    return list(set(prime_factors))
 if __name__ == "__main__":
     print ('Project Euler 3')
-    print (prime_factorization_by_division())
+    a = prime_factorization_by_division()
+    print (a)
+    #print (a)
--- python/p047.py
+++ python/p047.py
@@ -8,3 +8,29 @@
 # 646 = 2 x 17 x 19
 Find the first four consecutive integers to have four distinct prime factors each. What is the first of these numbers?
 """
+def prime_factorization_by_division(n):
+    prime_factors = []
+    c = 2
+    while n > 1:
+        if n % c == 0:
+            prime_factors.append(c)
+            while n % c == 0:
+                n //= c
+        else:
+            c += 1
+    return list(set(prime_factors))
+
+def compute():
+    for i in range(1000, 150000-3):
+        args = [i+k for k in range(4)]
+        prime_factors = all(len(prime_factorization_by_division(arg)) == 4 for arg in args)
+        if prime_factors:
+            return args[0]
+    return None
+
+if __name__ == "__main__":
+    print('Project Euler 47')
+    out = compute()
+    print(out)
+    
+    
