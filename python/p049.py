fc21e4863e887cb0717bf63fa81448aa17d28fe7
Umed Kakhkhorov <77145739+umedkahhorov@users.noreply.github.com>
2025-05-28 10:28:00 +0200

    Update p049.py

--- python/p049.py
+++ python/p049.py
@@ -14,3 +14,20 @@ permut_func = lambda digits, length: ["".join(d) for d in permutations(digits,le
 permutations_list2 = permut_func("1487", 4)
 print(permutations_list2[:4], len(permutations_list2))
 print(set(permutations_list1) == set(permutations_list2))
+# difference martix -- check 
+# Get absolute values
+abs_df = df.abs()
+# Compute the difference matrix
+perms = permutations_list2
+diff_table = [[row_val - col_val for col_val in perms] for row_val in perms]
+
+# Convert to DataFrame
+df = pd.DataFrame(diff_table, index=perms, columns=perms)
+# Loop through columns and show only actual repeated values
+for col in abs_df.columns:
+    col_vals = abs_df[col]
+    duplicates = col_vals[col_vals.duplicated(keep=False)]
+    
+    if not duplicates.empty:
+        print(f"\n🔁 Column {col} has repeated abs(diff) values:")
+        print(duplicates.sort_values())
