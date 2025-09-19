5c653e7e302e2f9668347953bf09500c73b7277a
Umed Kakhkhorov <77145739+umedkahhorov@users.noreply.github.com>
2025-09-19 13:49:17 +0200

    Update p051.py

--- python/p051.py
+++ python/p051.py
@@ -13,3 +13,34 @@ if __name__ == "__main__":
     plist = get_prime_list(100)
     compute()
     print(plist)
+from math import isqrt
+def get_prime_list(limit):
+    "sieve_of_eratosthenes returns a list of primes <= limit"
+    sieve = [True] * (limit + 1)
+    sieve[0] = sieve[1] = False
+    for i in range(2, isqrt(limit) + 1):
+        if sieve[i]:
+            for j in range(i*i, limit+1, i):
+                sieve[j] = False
+    return [i for i, is_prime in enumerate(sieve) if is_prime]
+def is_prime_list(limit):
+    "sieve_of_eratosthenes returns a list of true/false for primes <= limit"
+    sieve = [True] * (limit + 1)
+    sieve[0] = sieve[1] = False
+    for i in range(2, int(limit**0.5) + 1):
+        if sieve[i]:
+            for j in range(i*i, limit+1, i):
+                sieve[j] = False
+    return  sieve 
+def filter_by_digits(plist,ndigit):
+    # filter(function, iterable) 
+    func = lambda n: len(str(n)) == ndigit
+    filtered_plist = list(filter(func,plist))
+    return filtered_plist
+
+
+if __name__ == "__main__":
+    plist = get_prime_list(99999)
+    plist = filter_by_digits(plist,5)
+    f_replace_digit = lambda p,i,n: p[:i] + str(n) + p[i+1:]
+    print(plist)
